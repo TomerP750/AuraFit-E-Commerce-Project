28e3a70d0b9cb43f2d01fdf78503240f94c5d46f
TomerP750 <tomer.jcb65@gmail.com>
2025-04-24 21:55:05 +0300

    backend-init

---
+++ AuraFit-BackEnd/src/test/java/app/aurafitbackend/AuraFitBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package app.aurafitbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuraFitBackEndApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
