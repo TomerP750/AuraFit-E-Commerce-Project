540727ac6c8950429a573de0801c49d5fbf05329
TomerP750 <tomer.jcb65@gmail.com>
2025-05-08 17:03:42 +0300

    major bug fix

--- aurafit-frontend/src/main.tsx
+++ aurafit-frontend/src/main.tsx
@@ -2,6 +2,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import {Layout} from "./Components/LayoutArea/Layout/Layout.tsx";
 import {BrowserRouter} from "react-router-dom";
+import axios from 'axios';
+
+
+axios.interceptors.request.use(function (config) {
+    if (localStorage.token) { // if token exists
+        config.headers.Authorization = "Bearer " + localStorage.token;
+    }
+    return config;
+});
+
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
