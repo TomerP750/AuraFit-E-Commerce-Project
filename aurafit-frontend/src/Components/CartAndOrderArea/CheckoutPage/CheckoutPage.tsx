8910f80755546683c96787c51641a3f567875d94
TomerP750 <tomer.jcb65@gmail.com>
2025-05-13 22:20:18 +0300

    quick fix

--- aurafit-frontend/src/Components/CartAndOrderArea/CheckoutPage/CheckoutPage.tsx
+++ aurafit-frontend/src/Components/CartAndOrderArea/CheckoutPage/CheckoutPage.tsx
@@ -7,10 +7,14 @@ import {toast} from "react-toastify";
 import {CartDTO} from "../../../Models/DTOS/CartDTO.ts";
 import cartService from "../../../Services/CartService.ts";
 import {OrderItemCard} from "../OrderItemCard/OrderItemCard.tsx";
+import {useUserSelector} from "../../../Redux/hooks.ts";
+import {NavLink} from "react-router-dom";
+import {FiArrowLeft} from "react-icons/fi";
 
 export function CheckoutPage(): JSX.Element {
 
     const [cart, setCart] = useState<CartDTO>();
+    const user = useUserSelector(state => state.authSlice.user);
 
     useEffect(() => {
         cartService.getUserCart()
@@ -19,9 +23,11 @@ export function CheckoutPage(): JSX.Element {
     }, [])
 
     function placeOrder(data: CheckoutRequestDTO) {
-        orderService.placeOrder(data)
-            .then(res => toast.success("Order Placed, Thanks for shopping!"))
-            .catch((err) => toast.error(err.response.data))
+        if (user) {
+            orderService.placeOrder(data)
+                .then(res => toast.success("Order Placed, Thanks for shopping!"))
+                .catch((err) => toast.error(err.response.data))
+        }
     }
 
     if (!cart) {
@@ -30,14 +36,16 @@ export function CheckoutPage(): JSX.Element {
 
     return (
         <div className="flex flex-col lg:flex-row justify-center bg-white py-12">
-            <div className="w-9/10 flex justify-center items-center gap-5">
+            <div className="w-4/5 flex justify-center items-center gap-10">
                 {/* --------- Left Section --------- */}
-                <div className="w-2/3">
+                <div className="w-2/3 flex flex-col items-start gap-8">
+                    <NavLink className={"flex gap-1 items-center hover:underline"} to={"/cart"}><FiArrowLeft/> Back to cart</NavLink>
+                    <p className={"font-medium text-4xl"}>Checkout</p>
                     <CheckoutForm onSubmit={placeOrder}/>
                 </div>
 
                 {/* --------- Right Section --------- */}
-                <aside className="w-1/2 bg-neutral-200 text-black p-6 rounded-lg flex flex-col justify-between h-full">
+                <aside className="w-2/3 text-black p-6 border rounded-lg flex flex-col justify-between h-full">
                     <div>
                         <h2 className="text-2xl font-semibold mb-4">Order Summary</h2>
                         <div className="space-y-4 text-base">
@@ -63,7 +71,7 @@ export function CheckoutPage(): JSX.Element {
                     <button
                         form="checkoutForm"
                         type="submit"
-                        className="mt-6 bg-gray-800 hover:bg-gray-700 text-white font-semibold py-3 rounded w-full transition"
+                        className="cursor-pointer mt-6 bg-gray-800 hover:bg-gray-700 text-white font-semibold py-3 rounded w-full transition"
                     >
                         Place Order
                     </button>
